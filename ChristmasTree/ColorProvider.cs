19790deee49229ba62162ff7447f1a593ca37de9
Martivis <vanek-non@rambler.ru>
2022-12-31 23:14:21 +0300

    Добавьте файлы проекта.

---
+++ ChristmasTree/ColorProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChristmasTree
+{
+	internal static class ColorProvider
+	{
+		static Random random = new Random();
+		static ColorProvider()
+		{
+			random = new Random();
+		}
+
+		static public ConsoleColor GetRandomColor()
+		{
+			return (ConsoleColor)random.Next(0, Enum.GetNames(typeof(ConsoleColor)).Length);
+		}
+	}
+}
---
+++ ChristmasTree/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ChristmasTree
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Random random = new Random();
+			int length = 10;
+
+			for (int i = 0; i < length; i++)
+			{
+				for (int j = 0; j < length - i; j++)
+				{
+					Console.Write(" ");
+				}
+				for (int j = 0; j < i * 2 + 1; j++)
+				{
+					Console.ForegroundColor = random.Next(0, 10) > 5 ? ConsoleColor.DarkGreen : ColorProvider.GetRandomColor();
+
+					Console.Write("#");
+				}
+				Console.WriteLine();
+			}
+			for (int i = 0; i < length - 1; i++)
+			{
+				Console.Write(" ");
+			}
+			Console.ForegroundColor = ConsoleColor.DarkYellow;
+			for (int i = 0; i < length/10+2; i++)
+			{
+				Console.Write("|");
+			}
+			Console.ForegroundColor = ConsoleColor.White;
+		}
+	}
+}
